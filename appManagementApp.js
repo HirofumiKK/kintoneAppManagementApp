0c1645d5cdc445541c69a5a0923b220b750d206e
HirofumiKK <hikoichihara@gmail.com>
2019-07-29 13:22:30 -0700

    added some psuedo code

--- appManagementApp.js
+++ appManagementApp.js
@@ -8,6 +8,18 @@
 // with other parts that I can do.
 //*/
 
+/*
+Psuedo code
+1. on index.show call the initialize function
+2. declare funciton "initialize"
+3. declare a "getApps" funciton
+4. run getApps function to get apps data and assign the result to a new variable "allApps" 
+var allApps = getApps();
+5. declare "getExistingAppRecords" function
+6. run "getExistingAppRecords" function and assign the result to a new variable "existingRecords" 
+var existingRecords = getExistingAppRecords()
+*/
+
 (function(){
     'use strict';
     // return object of the space
@@ -160,25 +172,40 @@ async function f(){
 }
 
 
+function samplePromise(){
+    kintone.events.on('app.record.index.show', function(event) {
+        return kintone.api('/k/v1/records', 'GET',  {app: 20}).then(function(resp) {
+            alert("First pop-up");
+        }).then(function(){
+            alert("Second pop-up");//This should appear after the function before is resolved.
+        }).catch(function(error) {
+            var errmsg = 'There was an error when retrieving the data';      
+             //If an error is included in the response message, show it
+             if (error.message !== undefined){
+                errmsg += '\n' + error.message;
+             }
+             alert(errmsg);
+        });
+    });
+}
+
+
 /*
 process of fetching data and putting into records:
 full path to a data: resp.attachedApps[i].property
 full path in other words: admin -> spaces -> apps -> data
-
 the function addOrUpdate does everything for data within apps
 so I need another function to take care of outside of apps -> space
 although I could pass a space object instead of app object into this function,
 I feel like that would make the function less flexible, and I don't like that 
 since I plan to use this function as one of repeated used one to modify
 record. In that case, I'd need a parent function
-
 psudo code:
 General overview of my approach:
     get all spaces
     from each space, get all apps,
     from each app, get all data,
     add the required data from the apps as records
-
 List of required information and where/how they can be acquired:
     * Fetches app information within the platform
         * App Name: space -> app
@@ -190,19 +217,16 @@ List of required information and where/how they can be acquired:
         * Space ID: space
         * Space Name: space
         * Thread ID: space -> app
-
     * Fetches records information from each app:
         * Total numbers of records in each app: kintone.app.getQueryCondition
         * Most recent updated record: traverse through the update date
         * Most recent added record: the 0th index of the record array
         * Date of the most recent update: traverse through the update date 
         * Date of the most recent added: the 0th index of the record array
-
     * Status: Active / In-active / Delete
         * Active: Date of most recent updated record falls within one year
         * In-active: Date of most recent updated record went above one year
         * Delete: Record is no longer found in the API
-
     * Analysis:
         * App Level:
             * How many average log-ins for each app per week
@@ -212,6 +236,32 @@ List of required information and where/how they can be acquired:
             * # of Total Imports / Exports
 */
 
+
+//require("../../common/js/sweetAlert.min.js");
+//require("../../common/js/kintoneUtility.min.js");
+let EVENTS = "app.record.index.show";
+let currentAppId = kintone.app.getId();
+
+function getRecordsInfo(id) {
+    let appId = id === undefined ? currentAppId : id;
+    return new Promise(function(resolve) {
+        kintone.getAllRecordsByQuery({
+            app: appId,
+            query: kintone.app.getQueryCondition(),
+            fields: ['app_id', 'app_status', 'date_last_modified', 'user_last_modified'],
+            isGuest: false
+        }).then(function(response) {
+            // console.log(response);
+            resolve(response);
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }, function(error) {
+        console.log(error);
+    });
+}
+
+
     kintone.events.on("app.record.index.show", function(event){
         // var mySpace = getSpace(12);
         let spaceId = 12;
@@ -219,6 +269,7 @@ List of required information and where/how they can be acquired:
         kintone.api(kintone.api.url('/k/v1/space', true), 'GET', body, function(resp) {
             // success
             console.log(resp);
+            console.log(getRecordsInfo(176));
             let myApps = resp.attachedApps;
             let totalNumApps = myApps.length;
             // get data from each app and add records into this app
@@ -230,9 +281,17 @@ List of required information and where/how they can be acquired:
             // error
             console.log(error);
         });
-        let myApp = getApp(117);
-        console.log(kintone.app.getQueryCondition);
+        let promise = new Promise(resolve => {
+            resolve(getApp(117));
+        });
+        promise.then(
+            result => console.log(result),
+            error => alert(error)
+        );
+
+        console.log(getSpaceHelper());
+
+
         console.log("success");
     });
 })();
-
